7cb576892a10b0b2ef4531e30e8979f26e1f6763
PatricioCV91 <pa.carov@duocuc.cl>
2026-05-30 11:19:51 -0400

    cambios explicativos

---
+++ Funciones.py
@@ -0,0 +1,16 @@
+def sumarDosNumeros():
+    '''esta funcion permite sumar dos numeros, ingresados dentro de la función'''
+
+    num1= int(input("ingrese numero1: "))
+    num2= int(input("ingrese numero2: "))
+
+    suma = num1 + num2
+    print(f"la suma de {num1} + {num2} es = {suma}")
+
+def sumar(a,b):
+    '''esta funcion permite sumar dos numeros ingresados por parametros'''
+    suma = a+b
+    return suma
+
+
+
